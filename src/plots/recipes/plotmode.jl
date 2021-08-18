2a80b996d6af9f878bf4280756ac284a2acab287
csimal <csimal@student.unamur.be>
2021-08-18 16:34:06 +0200

    [Feature] Added title in plotmode

--- src/plots/recipes/plotmode.jl
+++ src/plots/recipes/plotmode.jl
@@ -1,12 +1,13 @@
 @userplot PlotMode
 
-struct PlotModeTheme
-    linecolor::Symbol
-    prefix::String
-    
+function get_mode_title(pseudo, approx, z, s)
+    prefix = pseudo ? "Pseudomode: " : "Eigenmode: "
+    rel = approx ? "\\approx" : "="
+    formatted = @sprintf("%15.2e", s)
+    infix = pseudo ? ("\$\\epsilon = " * formatted * "\$") : ("\$\\kappa(\\lambda) " * rel * formatted * "\$")
+    λ_str = @sprintf("%12g%+12gi", real(z), imag(z))
+    return prefix * infix * "\n  \$\\lambda = " * λ_str * "\$"
 end
-
-
 
 RecipesBase.@recipe f(p::PlotMode; pseudo::Bool, approx::Bool, verbosity)
 
@@ -19,11 +20,12 @@ RecipesBase.@recipe f(p::PlotMode; pseudo::Bool, approx::Bool, verbosity)
     q = U * q
     
     #showlog = sum(abs.(diff(q))) * (1/length(q)) >= 10*eps()
-
+    title_str = get_mode_title(pseudo, approx, z, SS[end])
     @series begin
         seriestype := :path
+        linecolor := (pseudo ? :magenta : :cyan)
         label := "realpt"
-        title := ""
+        title := title_str
         real.(q)
     end
     @series begin
